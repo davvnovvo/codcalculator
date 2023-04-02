5aa8cc20419a4b0bf817ae9f684e8ad62310e4fa
David <davidnovo1999@gmail.com>
2023-04-02 17:19:43 +0200

    Splash and Login created

--- app/src/main/java/com/codcalculator/login/LoginActivity.java
+++ app/src/main/java/com/codcalculator/login/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.codcalculator;
+package com.codcalculator.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.codcalculator.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
---
+++ app/src/main/java/com/codcalculator/splash/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.codcalculator.splash;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.codcalculator.login.LoginActivity;
+import com.codcalculator.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIMEOUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(intent);
+                // Finalizamos esta actividad para evitar que el usuario pueda volver atrás
+                finish();
+            }
+        }, SPLASH_TIMEOUT);
+    }
+
+    @Override
+    public void onBackPressed() {
+        // No hacemos nada
+    }
+}
