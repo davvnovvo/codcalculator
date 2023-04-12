2863910de1c2607b77614c11dcbf0ed2bce872ce
david.novo <davidnovo1999@gmail.com>
2023-04-12 13:58:14 +0200

    ForgotPassword class added

--- app/src/main/java/com/codcalculator/login/CreateUserActivity.java
+++ app/src/main/java/com/codcalculator/login/CreateUserActivity.java
@@ -3,29 +3,47 @@ package com.codcalculator.login;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.codcalculator.R;
+import com.codcalculator.main.MainActivity;
 
 public class CreateUserActivity extends AppCompatActivity {
+
+    ImageButton infoButton;
+    TextView textViewForgotPassword, textViewLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
 
-        Button infoButton = findViewById(R.id.buttonInfo);
-        infoButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(CreateUserActivity.this);
-                builder.setTitle("Términos y Condiciones");
-                builder.setMessage("Texto de ejemplo xD");
-                AlertDialog dialog = builder.create();
-                dialog.show();
-            }
+        infoButton = findViewById(R.id.buttonInfo);
+        textViewForgotPassword = findViewById(R.id.textViewForgotPassword);
+        textViewLogin = findViewById(R.id.textViewLogin);
+
+        infoButton.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(CreateUserActivity.this);
+            builder.setTitle("Términos y Condiciones");
+            builder.setMessage("Texto de ejemplo xD");
+            AlertDialog dialog = builder.create();
+            dialog.show();
         });
+
+        textViewForgotPassword.setOnClickListener(v -> {
+            Intent intent = new Intent(CreateUserActivity.this, ForgotPassword.class);
+            startActivity(intent);
+        });
+
+        textViewLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(CreateUserActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
     }
 }
