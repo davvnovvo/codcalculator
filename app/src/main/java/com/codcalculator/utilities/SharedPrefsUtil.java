ddd10a981c1b9799f5f8968fce81c536f6882f9e
david.novo <david.novo@inceo.com>
2023-04-21 14:45:40 +0200

    Inicio sesión con Google añadido

---
+++ app/src/main/java/com/codcalculator/utilities/SharedPrefsUtil.java
@@ -0,0 +1,34 @@
+package com.codcalculator.utilities;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+//Class to store and load SharedPreferences
+public class SharedPrefsUtil {
+
+    private static final String PREFS_NAME = "VALUES";
+
+    public static void saveBoolean(Context context, String key, boolean value) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    public static boolean getBoolean(Context context, String key) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return prefs.getBoolean(key, false);
+    }
+
+    public static void saveString(Context context, String key, String value) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(key, value);
+        editor.apply();
+    }
+
+    public static String getString(Context context, String key) {
+        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        return prefs.getString(key, "");
+    }
+}
